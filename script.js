d0103cf5bea29c97ab32e2ae918a70df85006c70
chhonmeily <chhonmeily@gmail.com>
2015-09-11 07:50:03 +0700

    Fix style and for mobile and tablet also add animation to the page

--- script.js
+++ script.js
@@ -1,18 +1,29 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-$(window).ready(function(){
-	$('#carousel-example-generic, #carousel-example-generic-1').carousel();
-	$("#middle-text").fadeOut();
-	$('.passion').fadeOut();
-	$(window).scroll(function(){
-		if($(window).scrollTop() > 500){
-			$("#middle-text").fadeIn("slow");
-			$('.passion').fadeIn("slow");
+$(document).ready(function(){
+	$('#carousel-top, #carousel-example-generic-1').carousel();
+	$('.search-box').hide();
+	$('.search-icon').on('click', function(e){
+		e.preventDefault();
+		$('.search-box').toggle(function(){
+			$('.search-box input[type="text"]').focus();
+		});
+		$('#top-content-home').toggleClass('animate-down');
+	});
+	$(window).on('scroll', function(){
+		if($(window).scrollTop() >= 50){
+			$('.header').addClass('navbar-fixed-top');
+			$('.header').fadeIn('slow').css({"box-shadow":"0px 5px 20px rgba(0,0,0,0.2)"})
+			$('.top-header-bar').hide();
+		}else{
+			$('.header').removeClass('navbar-fixed-top');
+			$('.top-header-bar').show();
 		}
 	});
+	$('#carousel-top .carousel-inner').swiperight(function(){
+		$(this).parent().carousel('prev');
+	});
+	$('#carousel-top .carousel-inner').swipeleft(function(){
+		$(this).parent().carousel('next');
+	});
 });
-	
+
 

e98d36ebc4aa6198c8fb728fdef8626b2eff2e39
meily <chhonmeily@gmail.com>
2015-11-12 15:30:17 +0700

    show error message when required field are empty

--- delivery-service/Resources/public/temp/js/profile.js
+++ delivery-service/Resources/public/temp/js/profile.js
@@ -6,8 +6,13 @@ var profile = (function(){
 		$('.change-password-form').submit(function(e){
 			$('.change-password-form input[required]').each(function(){
 				var allValue = $(this).val();
+				var newPassWord = $('#inputNewPassword').val();
+				var confirmPassWord = $('#inputPasswordAgain').val();
 				if(allValue == '' || allValue == null) {
 					$('.message-area .alert-danger').show();
+					e.preventDefault();
+				}else if ( newPassWord !== confirmPassWord ) {
+					$('.message-area .alert-danger').empty().text("Please fill in your new password again.").show();
 					e.preventDefault();
 				}
 			});
